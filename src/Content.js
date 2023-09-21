e59c04083af555724b6c1d4189145f9960a24de3
Chigozie Ofodike <ChigozieOfodike@gmail.com>
2023-09-21 16:21:20 -0400

    Learnt about event and added to react project app

--- src/Content.js
+++ src/Content.js
@@ -6,9 +6,28 @@ const Content = () => {
     const potentialIndex = Math.floor(Math.random() * names.length);
     return names[potentialIndex];
   };
+
+  const handleClick = () => {
+    console.log("You clicked it!");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
   return (
     <main>
-      <p>Hello, {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello, {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click it</button>
+      <br />
+
+      <p>Click events with functions:</p>
+      <button onClick={() => handleClick2(handleNameChange())}>Click it</button>
+      <br />
+
+      <p>Click events with events:</p>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   );
 };
